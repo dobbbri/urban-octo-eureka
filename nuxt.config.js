adfa14320efd781d13d3b737ed9e50718dfe4d61
Sergio Dobri <sergiodobri@gmail.com>
2021-08-13 11:20:16 -0300

    add tailwind and Daisyui

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,81 +1,43 @@
-
 export default {
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
+  target: "server",
 
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
+  plugins: [],
 
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
   components: true,
 
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
+  buildModules: ["nuxt-vite", "@nuxtjs/tailwindcss", "@nuxtjs/eslint-module"],
 
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://http.nuxtjs.org
-    '@nuxt/http'
-  ],
+  tailwindcss: {
+    viewer: false,
+  },
 
-  /*
-  ** Server Middleware
-  */
+  modules: ["@nuxt/http"],
+
   serverMiddleware: {
-    '/api': '~/api'
+    "/api": "~/api",
   },
 
   /*
-  ** For deployment you might want to edit host and port
-  */
+   ** For deployment you might want to edit host and port
+   */
   // server: {
   //  port: 8000, // default: 3000
   //  host: '0.0.0.0' // default: localhost
   // },
 
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  }
-}
+  build: {},
+};
